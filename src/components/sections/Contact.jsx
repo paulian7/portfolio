aeeec56e0144f304729125e93348f25f32315b54
paulian7 <paulianle7@gmail.com>
2025-09-14 00:13:19 -0700

    added starting files for fun & contact section

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,1 +1,17 @@
-// import { RevealOnScroll } from "../RevealOnScroll";
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Contact = () => {
+  return (
+    <section
+      id="contact"
+      className="min-h-screen flex flex-col items-center justify-center py-20 bg-white"
+    >
+      <RevealOnScroll>
+        {/* title */}
+        <h2 className="text-4xl font-bold mb-12 font-baloo text-black text-center">
+          Contact Me!
+        </h2>
+      </RevealOnScroll>
+    </section>
+  );
+};
