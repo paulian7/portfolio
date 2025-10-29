0eb50baa7c6870a09d75d42d505ccb9dbad9e41b
paulian7 <paulianle7@gmail.com>
2025-10-29 01:34:42 -0700

    polish nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,47 +9,55 @@ export const NavBar = ({ menuOpen, setMenuOpen }) => {
 
   return (
     <nav className="fixed top-0 w-full z-40 bg-white/80 backdrop-blur-lg drop-shadow-md">
-  {/* top container */}
-  <div className="max-w-7xl mx-auto px-6 md:px-16 lg:px-24">
-    {/* nav bar logo && hamburger functionality */}
-    <div className="flex justify-between items-center h-16">
-      {/* placeholder - #home */}
-      <a className="font-baloo text-2xl font-bold text-black" href="#home">
-        pl
-      </a>
+      {/* top container */}
+      <div className="max-w-7xl mx-auto px-6 md:px-16 lg:px-24">
+        {/* nav bar logo && hamburger functionality */}
+        <div className="flex justify-between items-center h-18">
+          {/* placeholder - #home */}
+          <a className="font-baloo text-2xl font-bold text-black" href="#home">
+            pl
+          </a>
 
-      {/* mobile nav bar */}
-      <div
-        className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
-        onClick={() => setMenuOpen((prev) => !prev)}
-      >
-        <RxHamburgerMenu size={"auto"} />
+          {/* mobile nav bar */}
+          <div
+            className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
+            onClick={() => setMenuOpen((prev) => !prev)}
+          >
+            <RxHamburgerMenu size={"auto"} />
+          </div>
+
+          {/* desktop nav bar */}
+          <div className="hidden md:flex items-center space-x-8">
+            <a
+              href="#about"
+              className="font-baloo font-medium text-lg text-black px-4 py-2 rounded-full hover:bg-[#0A7FBF]/10 transition duration-400"
+            >
+              about
+            </a>
+
+            <a
+              href="#projects"
+              className="font-baloo font-medium text-lg text-black px-4 py-2 rounded-full hover:bg-[#0A7FBF]/10 transition duration-400"
+            >
+              projects
+            </a>
+
+            <a
+              href="#fun"
+              className="font-baloo font-medium text-lg text-black px-4 py-2 rounded-full hover:bg-[#0A7FBF]/10 transition duration-400"
+            >
+              fun
+            </a>
+
+            <a
+              href="#contact"
+              className="font-baloo font-medium text-lg text-black px-4 py-2 rounded-full hover:bg-[#0A7FBF]/10 transition duration-400"
+            >
+              contact
+            </a>
+          </div>
+        </div>
       </div>
-
-      {/* desktop nav bar */}
-      <div className="hidden md:flex items-center space-x-8">
-        <a href="#about" className="group text-black transition duration-500">
-          about
-          <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-black/85 rounded-full"></span>
-        </a>
-
-        <a href="#projects" className="group text-black transition duration-500">
-          projects
-          <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-black/85 rounded-full"></span>
-        </a>
-
-        <a href="#fun" className="group text-black transition duration-500">
-          fun
-          <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-black/85 rounded-full"></span>
-        </a>
-
-        <a href="#contact" className="group text-black transition duration-500">
-          contact
-          <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-black/85 rounded-full"></span>
-        </a>
-      </div>
-    </div>
-  </div>
-</nav>
+    </nav>
   );
 };
